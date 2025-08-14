27364a1c60add0f51c54eaf4958abc58388bf775
hwellsss <1623571748@qq.com>
2025-08-14 17:27:41 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -109,4 +109,25 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return new PageResult(total,records);
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    public void startOrStop(Integer status, Long id) {
+        //update employee set status = ? where id = ?
+
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setStatus(status);
+
+//        Employee.builder()
+//                .status(status)
+//                .id(id)
+//                .build();
+
+        employeeMapper.update(employee);
+    }
 }
